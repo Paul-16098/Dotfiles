066225c2fe0d059c7617169f1a184e68311ae397
Paul-16098 <pl816098@gmail.com>
2026-08-21 12:07:22 +0800

    refactor(nushell): simplify Stop-Process and add pause
    Replace Get-Process | Stop-Process with direct Stop-Process call. Add pause command for user convenience after update completes.

--- dot_config/nushell/scripts/nu-selfupdate.ps1
+++ dot_config/nushell/scripts/nu-selfupdate.ps1
@@ -1,5 +1,7 @@
 Write-Output "Checking for NuShell updates..."
 
-Get-Process -Name nu | Stop-Process
+Stop-Process -Name nu
 
 cargo install --locked --git https://github.com/nushell/nushell.git nu nu_plugin_formats nu_plugin_polars nu_plugin_query -F full
+
+pause
