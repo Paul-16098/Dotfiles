4974227160697220276913c640fb72665c288374
Paul-16098 <pl816098@gmail.com>
2026-08-21 13:02:16 +0800

    refactor(nushell): separate NuShell and plugin installations
    Split combined cargo install command into two distinct operations for better modularity and clearer separation between main nu installation and optional plugins

--- dot_config/nushell/scripts/nu-selfupdate.ps1
+++ dot_config/nushell/scripts/nu-selfupdate.ps1
@@ -2,6 +2,7 @@ Write-Output "Checking for NuShell updates..."
 
 Stop-Process -Name nu
 
-cargo install --locked --git https://github.com/nushell/nushell.git nu nu_plugin_formats nu_plugin_polars nu_plugin_query -F full
+cargo install --locked --git https://github.com/nushell/nushell.git nu -F full
+cargo install --locked --git https://github.com/nushell/nushell.git nu_plugin_formats nu_plugin_polars nu_plugin_query
 
 pause
